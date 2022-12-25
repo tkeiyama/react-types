0088264f4fee9f989f2bb247b689a5f9c0e6f1de
tkeiyama <thomas18thx@gmail.com>
2022-12-25 16:03:09 +0900

    initial commit

---
+++ types/polymorphism.ts
@@ -0,0 +1,32 @@
+/**
+ * The code is originally from `https://github.com/ohansemmanuel/polymorphic-react-component`.
+ */
+import React from "react";
+
+/* ------------------------------
+ * Utility types
+ * ------------------------------*/
+export type PropsKeys<Element extends React.ElementType, Props> = keyof (AsProp<Element> & Props);
+
+export interface AsProp<Element extends React.ElementType> {
+  as?: Element;
+}
+
+export type Merge<P1 = {}, P2 = {}> = Omit<P1, keyof P2> & P2;
+
+export type PolymorphicRef<Element extends React.ElementType> = React.ComponentPropsWithRef<Element>["ref"];
+
+/* ------------------------------
+ * Polymorphic component types
+ * ------------------------------*/
+export type PolymorphicComponentProp<
+  Element extends React.ElementType,
+  Props = {},
+> =
+  & React.PropsWithChildren<Props & AsProp<Element>>
+  & Omit<React.ComponentPropsWithoutRef<Element>, PropsKeys<Element, Props>>;
+
+export type PolymorphicComponentPropWithRef<
+  Element extends React.ElementType,
+  Props = {},
+> = PolymorphicComponentProp<Element, Props> & { ref?: PolymorphicRef<Element> };
